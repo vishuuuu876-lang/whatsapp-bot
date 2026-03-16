1f9f0fa248815c78259521c2ddfff20e397d49bd
vishuuuu876-lang <vishuuuu876@gmail.com>
2026-03-16 13:24:12 +0530

    Update engine.js

--- games/engine.js
+++ games/engine.js
@@ -1,1 +1,40 @@
+export const activeGames = {}
 
+export function createGame(chat, gameType, host, mode) {
+
+activeGames[chat] = {
+type: gameType,
+host: host,
+mode: mode,
+players: [host],
+started: false,
+data: {}
+}
+
+}
+
+export function joinGame(chat, player) {
+
+if(!activeGames[chat]) return false
+
+if(!activeGames[chat].players.includes(player)){
+activeGames[chat].players.push(player)
+}
+
+return true
+}
+
+export function startGame(chat){
+
+if(!activeGames[chat]) return false
+
+activeGames[chat].started = true
+return true
+
+}
+
+export function endGame(chat){
+
+delete activeGames[chat]
+
+}
