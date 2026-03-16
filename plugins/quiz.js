7dd3aefedf7d7a3b5c21e4400b48e0fb4c8b36b3
vishuuuu876-lang <vishuuuu876@gmail.com>
2026-03-16 15:45:57 +0530

    Update quiz.js

--- plugins/quiz.js
+++ plugins/quiz.js
@@ -1,6 +1,6 @@
-import { games,createGame,joinGame,startGame,endGame } from "../games/engine.js"
+import { games, createGame, startGame, endGame } from "../games/engine.js"
 
-export default async function(client,message,args){
+export default async function(client, message, args){
 
 const chat = message.from
 const sender = message.author || message.from
@@ -11,29 +11,22 @@ if(!games[chat]){
 createGame(chat,"quiz",sender,mode)
 
 if(mode==="multi"){
-message.reply(`🧠 Quiz Lobby
+return message.reply(
+`🧠 Quiz Lobby
 
 .join to join
-.start to begin`)
-return
+.start to begin`
+)
 }
 
-startGame(chat)
+startGame(chat,sender)
 }
 
 let game = games[chat]
 
-if(message.body===".join"){
-joinGame(chat,sender)
-message.reply(`Player joined (${game.players.length})`)
-return
-}
+/* START QUESTION */
 
-if(message.body===".start"){
-
-if(sender!==game.host) return message.reply("Only host can start")
-
-startGame(chat)
+if(!game.data.answer){
 
 const res = await fetch("https://opentdb.com/api.php?amount=1&type=multiple")
 const data = await res.json()
@@ -42,17 +35,21 @@ let q = data.results[0]
 
 game.data.answer = q.correct_answer.toLowerCase()
 
-let options=[...q.incorrect_answers,q.correct_answer]
+let options = [...q.incorrect_answers, q.correct_answer]
 .sort(()=>Math.random()-0.5)
 
-message.reply(`🧠 Quiz
+await message.reply(
+`🧠 Quiz
 
 ${q.question}
 
-${options.join("\n")}`)
+${options.join("\n")}`
+)
 
 return
 }
+
+/* CHECK ANSWER */
 
 if(game.started){
 
@@ -60,11 +57,14 @@ if(game.mode==="multi" && !game.players.includes(sender)) return
 
 let answer = message.body.toLowerCase()
 
-if(answer===game.data.answer){
-message.reply(`🎉 ${sender.split("@")[0]} got it right!`)
-endGame(chat)
-}
+if(answer === game.data.answer){
 
+await message.reply(
+`🎉 ${sender.split("@")[0]} got it right!`
+)
+
+game.data.answer = null
+}
 }
 
 }
