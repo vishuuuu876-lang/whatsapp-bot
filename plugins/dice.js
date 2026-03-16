a8cbf17b4aa92b8f2f49ae638abd4ab2cd04497c
vishuuuu876-lang <vishuuuu876@gmail.com>
2026-03-16 15:24:53 +0530

    Update dice.js

--- plugins/dice.js
+++ plugins/dice.js
@@ -1,7 +1,14 @@
-export default async function(client, message){
+export default async function (client, message) {
 
-const dice = Math.floor(Math.random()*6)+1
+const dice = Math.floor(Math.random() * 6) + 1
 
-message.reply(`🎲 Dice rolled: ${dice}`)
+const diceEmoji = ["⚀","⚁","⚂","⚃","⚄","⚅"]
+
+await message.reply(
+`🎲 Dice Roll
+
+Number: ${dice}
+Face: ${diceEmoji[dice-1]}`
+)
 
 }
