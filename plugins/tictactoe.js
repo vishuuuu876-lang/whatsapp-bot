05e8f6508908b027a2ecf28bf3457fa33d57d183
vishuuuu876-lang <vishuuuu876@gmail.com>
2026-03-16 15:56:21 +0530

    Update tictactoe.js

--- plugins/tictactoe.js
+++ plugins/tictactoe.js
@@ -1,65 +1,69 @@
-let games = {}
+import { games, createGame, startGame, endGame } from "../games/engine.js"
 
-export default async function(client, message, args){
+export default async function(client,message,args){
 
 const chat = message.from
+const sender = message.author || message.from
 
-// start game
 if(!games[chat]){
 
-games[chat] = {
-board:["1","2","3","4","5","6","7","8","9"],
-turn:"X"
+createGame(chat,"tictactoe",sender,"single")
+startGame(chat,sender)
+
+games[chat].data = {
+board:["1","2","3","4","5","6","7","8","9"]
 }
 
-message.reply(`🎮 Tic Tac Toe
+return message.reply(`🎮 Tic Tac Toe
 
 1 | 2 | 3
+---------
 4 | 5 | 6
+---------
 7 | 8 | 9
 
-Send a number (1-9) to play.`)
-
-return
+Send a number (1-9)`)
 }
 
-// play move
 let game = games[chat]
+
+if(!game.data) return
 
 let move = parseInt(message.body)
 
-if(!move || move<1 || move>9){
-message.reply("Send a number between 1-9")
-return
+if(isNaN(move) || move < 1 || move > 9){
+return message.reply("Send a number between 1-9")
 }
 
-if(game.board[move-1]==="X" || game.board[move-1]==="O"){
-message.reply("❌ Position taken")
-return
+let board = game.data.board
+
+if(board[move-1] === "X" || board[move-1] === "O"){
+return message.reply("❌ Position already taken")
 }
 
-game.board[move-1] = "X"
+board[move-1] = "X"
 
-// simple bot move
-let empty = game.board
+/* BOT MOVE */
+
+let empty = board
 .map((v,i)=>v!=="X" && v!=="O" ? i : null)
 .filter(v=>v!==null)
 
-if(empty.length>0){
+if(empty.length > 0){
+
 let botMove = empty[Math.floor(Math.random()*empty.length)]
-game.board[botMove] = "O"
+board[botMove] = "O"
+
 }
 
-let b = game.board
-
-let board = `
-${b[0]} | ${b[1]} | ${b[2]}
+let view = `
+${board[0]} | ${board[1]} | ${board[2]}
 ---------
-${b[3]} | ${b[4]} | ${b[5]}
+${board[3]} | ${board[4]} | ${board[5]}
 ---------
-${b[6]} | ${b[7]} | ${b[8]}
+${board[6]} | ${board[7]} | ${board[8]}
 `
 
-message.reply(board)
+await message.reply(view)
 
 }
